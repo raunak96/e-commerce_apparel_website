aa367ba5907b78a8e51c533890ae089bdd9ec4f9
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-07-05 19:49:54 +0530

    initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+const Homepage=()=>(
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item"><div className="content"><h1 className="title">HATS</h1><spap className="subtitle">SHOP NOW</spap></div></div>
+            <div className="menu-item"><div className="content"><h1 className="title">JACKETS</h1><spap className="subtitle">SHOP NOW</spap></div></div>
+            <div className="menu-item"><div className="content"><h1 className="title">SNEAKERS</h1><spap className="subtitle">SHOP NOW</spap></div></div>
+            <div className="menu-item"><div className="content"><h1 className="title">WOMENS</h1><spap className="subtitle">SHOP NOW</spap></div></div>
+            <div className="menu-item"><div className="content"><h1 className="title">MENS</h1><spap className="subtitle">SHOP NOW</spap></div></div>
+        </div>
+    </div>
+);
+export default Homepage;
