d9dba9d9141b5a7201c46e063cf88e36cf1269be
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-07-09 21:06:56 +0530

    Cart-Icon now also shows total quantity on the icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,19 @@ import {connect} from 'react-redux';
 import {ReactComponent as ShoppingIcon} from "../../assets/shoppingBag.svg";
 import { toggleCart } from '../../redux/cart/cart.actions';
 
-const CartIcon=({setCartVisibility})=>(
+const CartIcon=({setCartVisibility,itemCount})=>(
     <div className="cart-icon" onClick={setCartVisibility}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
+
+const mapStateToProps = ({cart:{cartItems}})=>({
+    itemCount:cartItems.reduce((resultTillNow,cartItem)=>resultTillNow+cartItem.quantity ,0)
+})
 
 const mapDispatchToProps = dispatch => ({
     setCartVisibility: () => dispatch(toggleCart()),
 });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
