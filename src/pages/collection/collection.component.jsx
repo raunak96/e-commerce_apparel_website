3d6beb055ba01f422760b7bbbf463ceab6c8b61d
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-07-11 00:35:28 +0530

    Added Collection Component to view Data of Apparels category wise.It is rendered through Route /shop/:collectionId

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./collection.styles.scss";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+        <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">
+                {items.map((item) => (
+                    <CollectionItem key={item.id} item={item}></CollectionItem>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+    // the 2nd param is the props of this component i.e collection Component
+    collection: selectCollection(ownProps.match.params.collectionId)(state), // state is always required for selector, since it already had other param
+}); // , we passed state in this manner unlike other selector where state was the only param
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
 
 // This page displays the preview of collections in website
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({match}) => (
     <div className="shop-page">
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview}></Route>
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage}></Route>
     </div>
 );
 
