3ae6e5bc7af45c57e3a3cfaf58531ba860414971
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-07-11 14:38:35 +0530

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
+/* eslint-disable eqeqeq */
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV =="development")
+    middlewares.push(logger);
 
 const store = createStore(rootReducer,applyMiddleware(...middlewares));
 const persistedStore = persistStore(store);   //now we will be able to cache store data, we also have to make persist reducer in root-reducer 
