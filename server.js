266de21cc9e0e85892e9985a65376a7a05906291
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-07-17 17:21:52 +0530

    removed bug 2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app=require('express')(); 
+const express=require('express');
+const app=express(); 
 const cors=require('cors');
 const bodyParser = require('body-parser');
 const path= require('path');
